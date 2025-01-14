80cb900ebcc14b3b21d313733c0885db0a5d7b3c
Nikolai Kozlov <ru.dev47@kgk-global.com>
2025-01-14 21:10:28 +0300

    nop

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,11 +1,17 @@
 // export const API_BASE_URL = 'http://192.168.0.113:8443'
 // export const API_WS_URL = 'ws://192.168.0.113:8443'
+
 // export const API_BASE_URL = 'http://176.124.144.241:8443'
 // export const API_WS_URL = 'ws://176.124.144.241:8443'
+
 // export const API_BASE_URL = 'http://home.atlasd.ru:8443'
 // export const API_WS_URL = 'ws://home.atlasd.ru:8443'
-export const API_BASE_URL = 'http://10.0.3.178:8443'
-export const API_WS_URL = 'ws://10.0.3.178:8443'
+
+// export const API_BASE_URL = 'http://10.0.3.178:8443'
+// export const API_WS_URL = 'ws://10.0.3.178:8443'
+
+export const API_BASE_URL = 'http://10.8.0.2:8443'
+export const API_WS_URL = 'ws://10.8.0.2:8443'
 
 export const API_ENDPOINTS = {
   LOGIN: '/signin',
